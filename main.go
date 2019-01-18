39d618f2b2808cdff1389aa902ef6bde785b655f
178inaba <178inaba@users.noreply.github.com>
2019-01-18 23:47:14 +0900

    Add main.go

---
+++ main.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	"log"
+	"time"
+
+	"github.com/sclevine/agouti"
+)
+
+func main() {
+	log.SetFlags(log.Lshortfile)
+
+	wd := agouti.ChromeDriver(
+		agouti.ChromeOptions("args", []string{"headless", "disable-gpu"}),
+	)
+	if err := wd.Start(); err != nil {
+		log.Fatal(err)
+	}
+	defer wd.Stop()
+
+	p, err := wd.NewPage()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	if err := p.Navigate("https://s1.fujitv.co.jp/safe/ippon/judgement/index.html"); err != nil {
+		log.Fatal(err)
+	}
+
+	time.Sleep(time.Second)
+	if err := p.Screenshot("image.png"); err != nil {
+		log.Fatal(err)
+	}
+}
